5d1eff59eb0acb16212aaa7790c730ad7abc4eba
black1112525 <black1112525@gmail.com>
2025-11-02 13:58:36 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+from flask import Flask, request, abort
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
+
+app = Flask(__name__)
+
+# Renderに設定する環境変数から取得
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+channel_secret = os.getenv('LINE_CHANNEL_SECRET')
+
+if not channel_access_token or not channel_secret:
+    raise ValueError("環境変数が設定されていません。Renderのダッシュボードで設定してください。")
+
+handler = WebhookHandler(channel_secret)
+configuration = Configuration(access_token=channel_access_token)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    user_message = event.message.text
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=user_message)]
+            )
+        )
+
+if __name__ == "__main__":
+    port = int(os.getenv('PORT', 8080))
+    app.run(host="0.0.0.0", port=port)
