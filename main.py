d6674da23cfde7fdb1847474b4809b57a33b4242
black1112525 <black1112525@gmail.com>
2025-11-15 22:43:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -398,4 +398,6 @@ def home():
 # ========================
 if __name__ == "__main__":
     keep_alive()
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
